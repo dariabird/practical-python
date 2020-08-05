4d8993c9ff23b41b0451a76e8aef0598e51fb31f
Daria Lopteva <daria.lopteva@abbyy.com>
2020-08-05 21:54:08 +0300

    Bouncing ball problem solution

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+if __name__ == "__main__":
+    height = 100
+
+    for i in range(10):
+        height = height * 0.6
+        print("{} {}".format(i + 1, round(height, 2)))

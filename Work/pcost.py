1bc5226b809065e983281cbffd5eb5e832abe632
Daria Lopteva <daria.lopteva@abbyy.com>
2020-08-12 23:02:37 +0300

    Exercise 1.33 solution

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,28 @@
 # pcost.py
 #
-# Exercise 1.27
-if __name__ == "__main__":
-    with open('Data/portfolio.csv', 'rt') as f:
-        next(f) # to skip headers
+# Exercise 1.27 & 1.33
+import csv
+import sys
+
+
+def portfolio_cost(filename):
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        next(rows)
         total = 0.
-        for line in f:
-            row = line.split(',')
-            shares, price = int(row[1]), float(row[2])
-            total += shares * price
-        print("Total cost ", total)
+        for row in rows:
+            try:
+                shares, price = int(row[1]), float(row[2])
+                total += shares * price
+            except ValueError:
+                print("Couldn't parse", row)
+    return total
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+    else:
+        filename = 'Data/portfolio.csv'
+    cost = portfolio_cost(filename)
+    print('Total cost:', cost)

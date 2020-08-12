4f987f27bc24ebb4a8f633ac7e3227bfaf9a4a47
Daria Lopteva <daria.lopteva@abbyy.com>
2020-08-12 21:37:39 +0300

    Create interesting.py
    Some notes from the course

---
+++ Work/interesting.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    s = 'hello'
+    '''
+    dir() produces a list of all operations that can appear after the (.).
+    '''
+    print(dir(s))
+    '''
+    Use the help() command to get more information about a specific operation
+    '''
+    help(s.upper)
+

689cd04973f0b0e7aec978a9b3c5aa4d82b1634f
Daria Lopteva <daria.lopteva@abbyy.com>
2020-08-13 21:52:06 +0300

    Exercise 2.4: A list of tuples

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,28 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import sys
+
+
+def read_portfolio(filename):
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        portfolio = []
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+        return portfolio
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+    else:
+        filename = 'Data/portfolio.csv'
+    portfolio = read_portfolio(filename)
+    total = 0.
+    for name, share, price in portfolio:
+        total += share * price
+    print(total)

c686de10246fbd4cec274abb34ef2fc4584ef4ff
Daria Lopteva <daria.lopteva@abbyy.com>
2020-08-21 17:07:20 +0300

    List, Set, Dict Comprehensions

--- Work/report.py
+++ Work/report.py
@@ -74,3 +74,35 @@ if __name__ == '__main__':
 
     combined = holdings + holdings2
     print(combined)
+
+    cost = sum([s['shares'] * s['price'] for s in portfolio])
+    print(cost)
+    value = sum([s['shares'] * prices[s['name']] for s in portfolio])
+    print(value)
+
+    more100 = [s for s in portfolio if s['shares'] > 100]
+    print(more100)
+
+    msftibm = [s for s in portfolio if s['name'] in {'MSFT', 'IBM'}]
+    print(msftibm)
+
+    cost10k = [s for s in portfolio if s['shares'] * s['price'] > 10000]
+    print(cost10k)
+
+    names = {s['name'] for s in portfolio}
+    print(names)
+
+    portfolio_prices = {name: prices[name] for name in names}
+    print(portfolio_prices)
+
+    f = open('Data/portfoliodate.csv')
+    rows = csv.reader(f)
+    headers = next(rows)
+    # define a variable that lists the columns that you actually care about:
+    select = ['name', 'shares', 'price']
+    # Now, locate the indices of the above columns in the source CSV file:
+    indices = [headers.index(colname) for colname in select]
+    # Finally, read a row of data and turn it into a dictionary using a dictionary comprehension
+    portfolio = [{colname: row[index] for colname, index in zip(select, indices)} for row in rows]
+    print(portfolio)
+    f.close()

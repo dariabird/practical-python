7c979962410ac71e0aec1cf3546dbfc4e1e34001
Daria Lopteva <daria.lopteva@abbyy.com>
2020-08-23 11:44:18 +0300

    Exercise 3.1: Structuring a program as a collection of functions

--- Work/report.py
+++ Work/report.py
@@ -2,12 +2,16 @@
 #
 # Exercise 2.4
 import csv
-from collections import Counter
-from pprint import pprint
-import sys
 
 
-def read_portfolio(filename):
+def formatted_price(price: float) -> str:
+    return f'${price:.2f}'
+
+def read_portfolio(filename: str) -> list:
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    '''
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
@@ -23,7 +27,11 @@ def read_portfolio(filename):
     return portfolio
 
 
-def read_portfolio_new(filename):
+def read_portfolio_new(filename: str) -> list:
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price using dict comprehension.
+    '''
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
@@ -35,7 +43,7 @@ def read_portfolio_new(filename):
     return portfolio
 
 
-def read_prices(filename):
+def read_prices(filename: str) -> dict:
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         prices = {}
@@ -47,7 +55,7 @@ def read_prices(filename):
     return prices
 
 
-def make_report(portfolio, prices):
+def make_report(portfolio: list, prices: list) -> list:
     report = []
     for p in portfolio:
         t = (p['name'], p['shares'], p['price'], prices[p['name']] - p['price'])
@@ -55,8 +63,12 @@ def make_report(portfolio, prices):
     return report
 
 
-def formatted_price(price):
-    return f'${price:.2f}'
+def print_report(report: str):
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print(('{:>10s} ' * 4).format(*headers))
+    print(f'{"":_>10s} ' * 4)
+    for name, shares, price, change in report:
+        print(f'{name:>10s} {shares:>10d} {formatted_price(price):>10s} {change:>10.2f}')
 
 
 if __name__ == '__main__':
@@ -64,57 +76,7 @@ if __name__ == '__main__':
     # portfolio = read_portfolio('Data/portfoliodate.csv')
     prices = read_prices('Data/prices.csv')
     report = make_report(portfolio, prices)
+    print_report(report)
 
-    headers = ('Name', 'Shares', 'Price', 'Change')
-    print(('{:>10s} '*4).format(*headers))
-    print(f'{"":_>10s} '*4)
-    for name, shares, price, change in report:
-        print(f'{name:>10s} {shares:>10d} {formatted_price(price):>10s} {change:>10.2f}')
 
-    holdings = Counter()
-    for s in portfolio:
-        holdings[s['name']] += s['shares']
-    print(holdings)
 
-    print(holdings.most_common(3))  # Get three most held stocks
-
-    portfolio2 = read_portfolio('Data/portfolio2.csv')
-    holdings2 = Counter()
-    for s in portfolio2:
-        holdings2[s['name']] += s['shares']
-    print(holdings2)
-
-    combined = holdings + holdings2
-    print(combined)
-
-    cost = sum([s['shares'] * s['price'] for s in portfolio])
-    print(cost)
-    value = sum([s['shares'] * prices[s['name']] for s in portfolio])
-    print(value)
-
-    more100 = [s for s in portfolio if s['shares'] > 100]
-    print(more100)
-
-    msftibm = [s for s in portfolio if s['name'] in {'MSFT', 'IBM'}]
-    print(msftibm)
-
-    cost10k = [s for s in portfolio if s['shares'] * s['price'] > 10000]
-    print(cost10k)
-
-    names = {s['name'] for s in portfolio}
-    print(names)
-
-    portfolio_prices = {name: prices[name] for name in names}
-    print(portfolio_prices)
-
-    f = open('Data/portfoliodate.csv')
-    rows = csv.reader(f)
-    headers = next(rows)
-    # define a variable that lists the columns that you actually care about:
-    select = ['name', 'shares', 'price']
-    # Now, locate the indices of the above columns in the source CSV file:
-    indices = [headers.index(colname) for colname in select]
-    # Finally, read a row of data and turn it into a dictionary using a dictionary comprehension
-    portfolio = [{colname: row[index] for colname, index in zip(select, indices)} for row in rows]
-    print(portfolio)
-    f.close()

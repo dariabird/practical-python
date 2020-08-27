c78d2805e695fce5484ffe97718fe17fdaa665d0
Daria Lopteva <daria.lopteva@abbyy.com>
2020-08-27 21:16:40 +0300

    Exercise 4.2: Adding some Methods

--- Work/stock.py
+++ Work/stock.py
@@ -3,3 +3,17 @@ class Stock:
         self.name = name
         self.shares = shares
         self.price = price
+
+    def cost(self):
+        return self.price * self.shares
+
+    def sell(self, amount):
+        self.shares -= amount
+
+
+if __name__ == '__main__':
+    s = Stock('GOOG', 100, 490.10)
+    print(s.cost())
+    s.sell(25)
+    print(s.shares)
+    print(s.cost())

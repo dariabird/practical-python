6c9bc985007a85219d1c5b43706af5db1429177f
Daria Lopteva <daria.lopteva@abbyy.com>
2020-08-27 21:06:17 +0300

    Update interesting.py

--- Work/interesting.py
+++ Work/interesting.py
@@ -191,6 +191,7 @@ print(holdings['IBM']) # [ (50, 91.1), (100, 45.23) ]
 # Problem: We want a history of the last N things. Solution: Use a deque.
 from collections import deque
 history = deque(maxlen=10)
+filename = 'some_file_name'
 with open(filename) as f:
     for line in f:
         history.append(line)
@@ -202,43 +203,43 @@ print(holdings)
 
 print(holdings.most_common(3))  # Get three most held stocks
 
-    portfolio2 = read_portfolio('Data/portfolio2.csv')
-    holdings2 = Counter()
-    for s in portfolio2:
-        holdings2[s['name']] += s['shares']
-    print(holdings2)
+portfolio2 = read_portfolio('Data/portfolio2.csv')
+holdings2 = Counter()
+for s in portfolio2:
+    holdings2[s['name']] += s['shares']
+print(holdings2)
 
-    combined = holdings + holdings2
-    print(combined)
+combined = holdings + holdings2
+print(combined)
 
-    cost = sum([s['shares'] * s['price'] for s in portfolio])
-    print(cost)
-    value = sum([s['shares'] * prices[s['name']] for s in portfolio])
-    print(value)
+cost = sum([s['shares'] * s['price'] for s in portfolio])
+print(cost)
+value = sum([s['shares'] * prices[s['name']] for s in portfolio])
+print(value)
 
-    more100 = [s for s in portfolio if s['shares'] > 100]
-    print(more100)
+more100 = [s for s in portfolio if s['shares'] > 100]
+print(more100)
 
-    msftibm = [s for s in portfolio if s['name'] in {'MSFT', 'IBM'}]
-    print(msftibm)
+msftibm = [s for s in portfolio if s['name'] in {'MSFT', 'IBM'}]
+print(msftibm)
 
-    cost10k = [s for s in portfolio if s['shares'] * s['price'] > 10000]
-    print(cost10k)
+cost10k = [s for s in portfolio if s['shares'] * s['price'] > 10000]
+print(cost10k)
 
-    names = {s['name'] for s in portfolio}
-    print(names)
+names = {s['name'] for s in portfolio}
+print(names)
 
-    portfolio_prices = {name: prices[name] for name in names}
-    print(portfolio_prices)
+portfolio_prices = {name: prices[name] for name in names}
+print(portfolio_prices)
 
-    f = open('Data/portfoliodate.csv')
-    rows = csv.reader(f)
-    headers = next(rows)
-    # define a variable that lists the columns that you actually care about:
-    select = ['name', 'shares', 'price']
-    # Now, locate the indices of the above columns in the source CSV file:
-    indices = [headers.index(colname) for colname in select]
-    # Finally, read a row of data and turn it into a dictionary using a dictionary comprehension
-    portfolio = [{colname: row[index] for colname, index in zip(select, indices)} for row in rows]
-    print(portfolio)
-    f.close()
+f = open('Data/portfoliodate.csv')
+rows = csv.reader(f)
+headers = next(rows)
+# define a variable that lists the columns that you actually care about:
+select = ['name', 'shares', 'price']
+# Now, locate the indices of the above columns in the source CSV file:
+indices = [headers.index(colname) for colname in select]
+# Finally, read a row of data and turn it into a dictionary using a dictionary comprehension
+portfolio = [{colname: row[index] for colname, index in zip(select, indices)} for row in rows]
+print(portfolio)
+f.close()

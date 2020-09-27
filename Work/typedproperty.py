1b7eae1ee6c14106b634e98ba475ef909b8ebe16
Daria Lopteva <daria.lopteva@abbyy.com>
2020-09-27 15:16:09 +0300

    Exercise 7.7: Using Closures to Avoid Repetition

---
+++ Work/typedproperty.py
@@ -0,0 +1,30 @@
+def typedproperty(name, expected_type):
+    private_name = '_' + name
+    @property
+    def prop(self):
+        return getattr(self, private_name)
+
+    @prop.setter
+    def prop(self, value):
+        if not isinstance(value, expected_type):
+            raise TypeError(f'Expected {expected_type}')
+        setattr(self, private_name, value)
+
+    return prop
+
+
+class Stock:
+    name = typedproperty('name', str)
+    shares = typedproperty('shares', int)
+    price = typedproperty('price', float)
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+
+if __name__ == '__main__':
+    s = Stock('IBM', 50, 91.1)
+    print(s)
+    s.shares = '100'
